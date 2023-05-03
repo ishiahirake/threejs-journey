f56691ec45c15cea1503fdab24b90d0f5e547caf
ishiahirake <ishiahirake@gmail.com>
2023-05-03 21:16:34 +0900

    feat: add 10 debug ui.

---
+++ chapters/01-basic/10-debug-ui/script.ts
@@ -0,0 +1,116 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GUI } from "lil-gui";
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+  // for safari, this also work on version 16.4+
+  // see https://caniuse.com/?search=requestFullscreen
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
+
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = (event.clientY / sizes.height - 0.5) * -1;
+});
+
+const scene = new THREE.Scene();
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Camera
+
+const camera = new THREE.PerspectiveCamera(90, sizes.width / sizes.height);
+
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+// camera.position.set(2, 2, 2);
+camera.position.z = 3;
+// camera.lookAt(mesh.position);
+scene.add(camera);
+
+// Renderer
+const canvas: HTMLCanvasElement = document.querySelector("#c")!;
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+// controls.enabled = false;
+controls.enableDamping = true;
+
+const clock = new THREE.Clock();
+
+// DEBUG UI
+const gui = new GUI();
+gui.add(mesh.position, "x").min(-3).max(3).step(0.01);
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01);
+gui.add(mesh.position, "z").min(-3).max(3).step(0.01);
+gui.add(mesh, "visible");
+
+gui.add(material, "wireframe");
+gui.addColor(material, "color");
+
+const myObject = {
+  foo: () => {
+    alert("foo");
+  },
+};
+
+gui.add(myObject, "foo").name("function");
+
+function tick() {
+  // Update objects
+  // mesh.rotation.y = clock.getElapsedTime();
+
+  // Update camera
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
+
+  controls.update();
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(tick);
+}
+
+tick();
